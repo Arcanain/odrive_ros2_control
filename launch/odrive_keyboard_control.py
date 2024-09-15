671b6ed4caf1d6080edb37e4c8db872f13ba2c28
tomtomApp <mtom0809tom0809@gmail.com>
2024-09-15 21:53:20 +0900

    [add]odrive_odom_pub

--- launch/odrive_keyboard_control.py
+++ launch/odrive_keyboard_control.py
@@ -6,14 +6,36 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    file_path = os.path.expanduser('~/ros2_ws/src/arcanain_simulator/urdf/mobile_robot.urdf.xml')
     package_dir = get_package_share_directory('odrive_ros2_control')
     rviz = os.path.join(package_dir, 'rviz', 'odom_publish.rviz')
+    with open(file_path, 'r') as file:
+        robot_description = file.read()
 
     return LaunchDescription([
         Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='both',
+            parameters=[{'robot_description': robot_description}]
+        ),
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            output='both',
+            parameters=[{'joint_state_publisher': robot_description}]
+        ),
+        Node(
             package='odrive_ros2_control',
-            executable='control_odrive',
-            name='odrive_twist_driver',
+            executable='control_odrive_and_odom_pub',
+            name='odrive_odom_pub',
+            output='screen'
+        ),
+        Node(
+            package='arcanain_simulator',
+            executable='odrive_odometry_pub',
             output='screen'
         ),
         Node(
